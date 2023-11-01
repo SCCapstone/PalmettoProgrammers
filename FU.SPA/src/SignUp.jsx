2dc98f338fcf9a4c587e9fe8ba8d9e1a9a5640f5
Aaron Keys <106256122+AaronKeys@users.noreply.github.com>
2023-11-01 12:58:24 -0400

    Update SignUp.jsx
    Cleaned up the import statements

--- FU.SPA/src/SignUp.jsx
+++ FU.SPA/src/SignUp.jsx
@@ -1,18 +1,7 @@
 import * as React from 'react';
-import Avatar from '@mui/material/Avatar';
-import Button from '@mui/material/Button';
-import CssBaseline from '@mui/material/CssBaseline';
-import TextField from '@mui/material/TextField';
-import FormControlLabel from '@mui/material/FormControlLabel';
-import Checkbox from '@mui/material/Checkbox';
-import Link from '@mui/material/Link';
-import Grid from '@mui/material/Grid';
-import Box from '@mui/material/Box';
-import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
-import Typography from '@mui/material/Typography';
-import Container from '@mui/material/Container';
+import { Button, TextField, Link, Box, Container, Typography, CssBaseline, Avatar, FormControlLabel, Icon, Grid, Checkbox } from '@mui/material';
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined'; // Replace with logo eventually
 import { createTheme, ThemeProvider } from '@mui/material/styles';
-import { Icon } from '@mui/material';
 
 
 // TODO remove, this demo shouldn't need to reset the theme.
