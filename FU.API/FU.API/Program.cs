75f94ce1c38ed3197a703425e56654344a77a406
James Pretorius <jamesmpretorius@gmail.com>
2023-11-04 15:07:04 -0400

    Fix formatting

--- FU.API/FU.API/Program.cs
+++ FU.API/FU.API/Program.cs
@@ -85,7 +85,9 @@ if (!app.Environment.IsDevelopment())
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
     app.UseHttpsRedirection();
-else {
+}
+else
+{
     app.UseSwagger();
     app.UseSwaggerUI();
 }
