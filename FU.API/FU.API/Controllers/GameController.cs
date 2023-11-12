a24b1f8d4f2892f6a48f6f14c3ed035c534fa73d
evan-scales <evan.m.scales@gmail.com>
2023-11-12 16:36:05 -0500

    121: Starting work

---
+++ FU.API/FU.API/Controllers/GameController.cs
@@ -0,0 +1,16 @@
+﻿namespace FU.API.Controllers
+{
+    using Microsoft.AspNetCore.Http;
+    using Microsoft.AspNetCore.Mvc;
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GameController : ControllerBase
+    {
+
+        // CREATE Game (Authorized)
+        // READ Game (Partial match)
+        // UPDATE Game (Authorized, Admin)
+        // DELETE Game (Authorized, Admin)
+    }
+}
---
+++ FU.API/FU.API/Controllers/TagController.cs
@@ -0,0 +1,15 @@
+﻿namespace FU.API.Controllers
+{
+    using Microsoft.AspNetCore.Http;
+    using Microsoft.AspNetCore.Mvc;
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TagController : ControllerBase
+    {
+        // CREATE Tag (Authorized)
+        // READ Tag (Partial match)
+        // UPDATE Tag (Authorized, Admin)
+        // DELETE Tag (Authorized, Admin)
+    }
+}
