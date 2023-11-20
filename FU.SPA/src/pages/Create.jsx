e14af00eb889d12fa1eaf9a4bc7be35e6d523b5a
AaronKeys <alkeys89@gmail.com>
2023-11-19 20:37:26 -0500

    rough draft of the create post section completed with the create group, but not displaying, yet

--- FU.SPA/src/pages/Create.jsx
+++ FU.SPA/src/pages/Create.jsx
@@ -14,15 +14,15 @@ const defaultTheme = createTheme();
  * The nav bar would go to Create Post first, then would go to the CreateGroup
  * section of the Create page, when it has been clicked on.
  */
-function CreatePost() { //This should be a redirect/nav button or just be a label, so as not to reset values in textfields.
-  const handleCreatePost = (event) => {
-    event.preventDefault();
-    const data = new FormData(event.currentTarget);
-    console.log({
-      //data to be collected from user
-    });
-  }
-};
+// function CreatePost() { //This should be a redirect/nav button or just be a label, so as not to reset values in textfields.
+//   const handleCreatePost = (event) => {
+//     event.preventDefault();
+//     const data = new FormData(event.currentTarget);
+//     console.log({
+//       //data to be collected from user
+//     });
+//   }
+// };
 
 function CreateGroup() { //This should be a redirect/nav button or just be a label, so as not to reset values in textfields.
   const handleCreatePost = (event) => {
@@ -39,7 +39,7 @@ function RadioButtonsGroup() {
       <FormLabel id="demo-radio-buttons-group-label">Communications</FormLabel>
       <RadioGroup
         aria-labelledby="demo-radio-buttons-group-label"
-        defaultValue="female"
+        defaultValue=""
         name="radio-buttons-group"
       >
         <FormControlLabel value="Mic" control={<Radio />} label="Mic" />
@@ -72,10 +72,19 @@ export default function Create() {
     });
   };
 
+function createGroupButton() {
+  const handleCreateGroup = (event) => {
+    event.preventDefault();
+    createGroup();
+  }
+}
 
-//Design of the page
+
+
+//START OF PAGE CODE, WHICH I'M CHANGING TO CREATE GROUP, BEFORE MAKING IT A BUTTON FUNCTION
+function createGroup() {
   return (
-    <ThemeProvider theme={defaultTheme}>
+    <ThemeProvider theme={createTheme}>
       <Container component="main" maxWidth="xs">
         <CssBaseline />
         <Box
@@ -110,17 +119,17 @@ export default function Create() {
                 marginLeft: 2,
               }}>
               <Typography component="h1" variant="h6">
-                Game (Post Title)
+                Group Name
               </Typography>
               </Box>
               <Grid item xs = {12}>
                 <TextField
                   required //may want to get rid of this and just check if it's empty when clicking create button.
                   fullWidth
-                  id="searchGames"
-                  label="Search Games" //might want to put a Search icon in front, if we can figure it out.
-                  type="searchGames"
-                  name="searchGames"
+                  id="groupName"
+                  label="Name here" //might want to put a Search icon in front, if we can figure it out.
+                  type="groupName"
+                  name="groupName"
                   autofocus
                 />
               </Grid>
@@ -201,44 +210,6 @@ export default function Create() {
                   autoComplete="searchHashtags"
                 /> {/* Need to put checkboxes under the hashtag section, which will display*/}
               </Grid>
-
-              <Box
-              sx={{
-                marginTop: 3,
-                display: 'flex',
-                marginLeft: 2,
-                marginRight: 2.5,
-              }}>
-              <Typography component="h1" variant="h6"> {/* Need to have 2 radius buttons below for 'Any' and 'Between' */}
-                Number of Players Needed
-              </Typography>
-              </Box>
-              <Grid item xs={12} sm={4}>{/* Might set default value to 1*/}
-                <TextField 
-                  name="minNum"
-                  fullWidth
-                  id="minNum"
-                  label="Min Number"
-                />
-              </Grid>
-              <Box
-              sx={{
-                marginLeft: 2,
-                marginTop: 3,
-                display: 'flex',
-              }}>
-              <Typography component="h1" variant="h6">
-                and
-              </Typography>
-              </Box>
-              <Grid item xs={12} sm={4}>{/* Might set default value to 3*/}
-                <TextField
-                  fullWidth
-                  id="maxNum"
-                  label="Max Number"
-                  name="maxNum"
-                />
-              </Grid>
               <Box
               sx={{
                 marginTop: 3,
@@ -252,17 +223,37 @@ export default function Create() {
               </Box>
               <Grid item xs={12} sm={6} marginTop={2}>
                 <TextareaAutosize aria-setsize={300}>
-
                 </TextareaAutosize>
               </Grid>
             </Grid>
+            <Box
+              sx={{
+                marginTop: 3,
+                display: 'flex',
+                marginRight: 2.5,
+              }}>
+              <Typography component="h1" variant="h6"> {/* Need to have 2 radius buttons below for 'Any' and 'Between' */}
+              Favorite Games
+              </Typography>
+              </Box>
+              <Grid item xs={12} sm={6} marginTop={2}>
+              <TextField
+                  fullWidth
+                  id="FavoriteGames"
+                  label="Search games" //might want to put a Search icon in front, if we can figure it out.
+                  type="FavoriteGames"
+                  name="FavoriteGames"
+                  autofocus
+                />
+              </Grid>
+            
             <Button
               type="submit"
               fullWidth
               variant="contained"
               sx={{ mt: 3, mb: 2 }}
             >
-              Create
+              Create Group
             </Button>
           </Box>
         </Box>
@@ -270,4 +261,212 @@ export default function Create() {
       </Container>
     </ThemeProvider>
   );
+  }
 }
+
+function createPostButton() {
+  const handleCreatePost = (event) => {
+    event.preventDefault();
+    createPost();
+  }
+}
+
+//Design of the page
+function CreatePost() { // going to use in another function for clicking
+    return (
+        <ThemeProvider theme={createTheme}>
+          <Container component="main" maxWidth="xs">
+            <CssBaseline />
+            <Box
+            sx={{
+              marginTop: 8,
+              display: 'flex',
+              flexDirection: 'column',
+              alignItems: 'left',
+            }}
+            >
+
+            </Box>
+            <Box
+              sx={{
+                marginTop: 8,
+                display: 'flex',
+                flexDirection: 'column',
+                alignItems: 'center',
+              }}
+            >
+              <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+                {/* <LockOutlinedIcon /> */}
+              </Avatar>
+              <Typography component="h1" variant="h5">
+                Create
+              </Typography>
+              <Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 3 }}>
+                <Grid container spacing={2}>
+                <Box
+                  sx={{
+                    display: 'flex',
+                    marginLeft: 2,
+                  }}>
+                  <Typography component="h1" variant="h6">
+                    Game (Post Title)
+                  </Typography>
+                  </Box>
+                  <Grid item xs = {12}>
+                    <TextField
+                      required //may want to get rid of this and just check if it's empty when clicking create button.
+                      fullWidth
+                      id="searchGames"
+                      label="Search Games" //might want to put a Search icon in front, if we can figure it out.
+                      type="searchGames"
+                      name="searchGames"
+                      autofocus
+                    />
+                  </Grid>
+
+                  <Box
+                  sx={{
+                    marginTop: 3,
+                    display: 'flex',
+                    marginLeft: 2,
+                    marginRight: 2.5,
+                  }}>
+                  <Typography component="h1" variant="h6">
+                    Time:
+                  </Typography>
+                  </Box>
+                  <Grid item xs={12} sm={4}>
+                    <TextField
+                      name="startTime"
+                      fullWidth
+                      id="startTime"
+                      label="Start Time"
+                    />
+                  </Grid>
+                  <Box
+                  sx={{
+                    marginLeft: 2,
+                    marginTop: 3,
+                    display: 'flex',
+                  }}>
+                  <Typography component="h1" variant="h6">
+                    and
+                  </Typography>
+                  </Box>
+                  <Grid item xs={12} sm={4}>
+                    <TextField
+                      fullWidth
+                      id="endTime"
+                      label="End Time"
+                      name="endTime"
+                    />
+                  </Grid>
+                  <Box
+                  sx={{
+                    marginTop: 3,
+                    display: 'flex',
+                    marginLeft: 2,
+                    marginRight: 2.5,
+                  }}>
+                  <Typography component="h1" variant="h6"> 
+                    Communication
+                  </Typography>
+                  </Box> {/* Need to place radius buttons under the Communication */}
+                  <Grid item xs={12} sm={5} marginTop={2}>
+                    <label>Any</label>
+                    <Radio>Any</Radio>
+                    <label>Mic</label>
+                    <Radio>Mic</Radio>
+                    <label>No Mic</label>
+                    <Radio>No Mic</Radio>
+                  </Grid>
+                  <Box
+                  sx={{
+                    marginTop: 3,
+                    display: 'flex',
+                    marginLeft: 2,
+                    marginRight: 2.5,
+                  }}>
+                  <Typography component="h1" variant="h6"> 
+                    Hashtags
+                  </Typography>
+                  </Box>
+                  <Grid item xs={12}>
+                    <TextField
+                      fullWidth
+                      id="searchHashtags"
+                      label="Search Hashtags"
+                      name="searchHashtags"
+                      autoComplete="searchHashtags"
+                    /> {/* Need to put checkboxes under the hashtag section, which will display*/}
+                  </Grid>
+
+                  <Box
+                  sx={{
+                    marginTop: 3,
+                    display: 'flex',
+                    marginLeft: 2,
+                    marginRight: 2.5,
+                  }}>
+                  <Typography component="h1" variant="h6"> {/* Need to have 2 radius buttons below for 'Any' and 'Between' */}
+                    Number of Players Needed
+                  </Typography>
+                  </Box>
+                  <Grid item xs={12} sm={4}>{/* Might set default value to 1*/}
+                    <TextField 
+                      name="minNum"
+                      fullWidth
+                      id="minNum"
+                      label="Min Number"
+                    />
+                  </Grid>
+                  <Box
+                  sx={{
+                    marginLeft: 2,
+                    marginTop: 3,
+                    display: 'flex',
+                  }}>
+                  <Typography component="h1" variant="h6">
+                    and
+                  </Typography>
+                  </Box>
+                  <Grid item xs={12} sm={4}>{/* Might set default value to 3*/}
+                    <TextField
+                      fullWidth
+                      id="maxNum"
+                      label="Max Number"
+                      name="maxNum"
+                    />
+                  </Grid>
+                  <Box
+                  sx={{
+                    marginTop: 3,
+                    display: 'flex',
+                    marginLeft: 2,
+                    marginRight: 2.5,
+                  }}>
+                  <Typography component="h1" variant="h6"> {/* Need to have 2 radius buttons below for 'Any' and 'Between' */}
+                    Description
+                  </Typography>
+                  </Box>
+                  <Grid item xs={12} sm={6} marginTop={2}>
+                    <TextareaAutosize aria-setsize={300}>
+                    </TextareaAutosize>
+                  </Grid>
+                </Grid>
+                <Button
+                  type="submit"
+                  fullWidth
+                  variant="contained"
+                  sx={{ mt: 3, mb: 2 }}
+                >
+                  Create Post
+                </Button>
+              </Box>
+            </Box>
+
+          </Container>
+        </ThemeProvider>
+      );
+    }
+  //removed brace here
