a9756b3f0308bb723484c83aceeb5d1c3c0ee765
James Pretorius <jamesmpretorius@gmail.com>
2023-11-30 14:42:01 -0500

    Fix posts component

--- FU.SPA/src/components/Posts.jsx
+++ FU.SPA/src/components/Posts.jsx
@@ -1,38 +1,35 @@
-import React  from 'react';
+import React from 'react';
 import { Button, Card, CardActions, CardContent, Typography } from '@mui/material';
 
 
-const Posts = ({posts}) => {
+const Posts = ({ posts }) => {
 
   return (
     <div style={{ display: 'flex', flexWrap: 'wrap', gap: '16px' }}>
-    <ul>
-  {posts.map((post) => (
-      <li key={post.Id}>{post.Title}
-        <Card sx={{ minWidth: 345, maxWidth: 345 }}>
-        <CardContent>
-        <Typography variant="body5" color="text.primary">
-          {post.Creator}
-        </Typography>
-        <Typography gutterBottom variant="h3" component="div">
-          {post.Title}
-        </Typography>
-        <Typography variant="body2" color="text.secondary">
-         {post.StartTime}
-        </Typography>
-        <Typography variant="body1" color="black">
-          {post.Description}
-        </Typography>
-      </CardContent>
-      <CardActions>
-        <Button size="small">Join</Button>
-        <Button size="small">View</Button>
-      </CardActions>
-    </Card>
-    </li>
-        ))}
-</ul>
-</div>
-);
+      {posts.map((post) => (
+        <Card key={post.id}>
+          {post.title}
+          <CardContent>
+            <Typography variant="body5" color="text.primary">
+              {post.creator}
+            </Typography>
+            <Typography gutterBottom variant="h3" component="div">
+              {post.title}
+            </Typography>
+            <Typography variant="body2" color="text.secondary">
+              {post.startTime}
+            </Typography>
+            <Typography variant="body1" color="black">
+              {post.description}
+            </Typography>
+          </CardContent>
+          <CardActions>
+            <Button size="small">Join</Button>
+            <Button size="small">View</Button>
+          </CardActions>
+        </Card>
+      ))}
+    </div>
+  );
 };
 export default Posts;
