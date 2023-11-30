a786f2d96ab6013baa8603bb6801b04c9d36595b
James Pretorius <jamesmpretorius@gmail.com>
2023-11-30 18:38:07 -0500

    Add keyword searching

--- FU.SPA/src/components/pages/Discover.jsx
+++ FU.SPA/src/components/pages/Discover.jsx
@@ -1,7 +1,66 @@
-import { useState } from "react";
+import { TextField } from "@mui/material";
+import { useEffect, useState } from "react";
+import SearchService from "../../services/searchService";
+import Posts from "../Posts";
+import "./Discover.css"
 
 export default function Discover() {
-    const [posts, setPosts] = useState([]);
+  const [posts, setPosts] = useState([]);
+  const [searchText, setSearchText] = useState("");
 
-    return <h1>Discover</h1>;
+  useEffect(() => {
+    submitSearch();
+  }, []);
+
+  const submitSearch = async () => {
+    console.log("Searching");
+
+    const query = {
+      keywords: searchText
+    };
+
+    const response = await SearchService.searchPosts(query);
+    setPosts(response);
+  };
+
+  return (
+    <div>
+      <SearchBar onSearchText={searchText} onSearchChange={t => setSearchText(t)} onSearchSubmit={submitSearch} />
+      <Posts posts={posts} />
+    </div>
+  );
+}
+
+function SearchBar({ onSearchText, onSearchChange, onSearchSubmit, onSortChange }) {
+  function onKeyDown(event) {
+    if (event.key === "Enter") {
+      onSearchSubmit()
+    }
+  }
+
+  return (
+    <div id='search-bar'>
+      <TextField
+        id="outlined-basic"
+        label="Search"
+        variant="outlined"
+        value={onSearchText}
+        onChange={e => onSearchChange(e.target.value)}
+        onKeyDown={onKeyDown}
+      />
+    </div>
+  )
+}
+
+function FilterSidebar() {
+  const [filters, setFilters] = useState({
+    tags: [],
+    games: [],
+    afterDateTime: "",
+    keywords: "",
+    sort: "",
+    limit: 20,
+    offset: 0,
+    minPlayers: 0,
+  });
 }
--- FU.SPA/src/services/searchService.js
+++ FU.SPA/src/services/searchService.js
@@ -1,13 +1,17 @@
 const API_BASE_URL = import.meta.env.VITE_API_URL;
 
-const searchPosts = async () => {
-    const response = await fetch(`${API_BASE_URL}/search/posts`);
 
-    if (!response.ok) {
-        throw new Error("Error fetching posts");
-    }
+/*
+  params = {
+    keywords: "",
+  }
+*/
+const searchPosts = async (query) => {
+  let queryString = "";
+  queryString += "keywords=" + encodeURIComponent(query.keywords);
+  const response = await fetch(`${API_BASE_URL}/search/posts?${queryString}`);
 
-    return await response.json();
+  return await response.json();
 };
 
 const SearchService = { searchPosts };
