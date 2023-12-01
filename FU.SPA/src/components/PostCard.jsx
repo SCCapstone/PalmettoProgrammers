6d489737aac9e6ec265810edb2391d0fca9436e9
James Pretorius <jamesmpretorius@gmail.com>
2023-12-01 15:32:13 -0500

    Fix indent size

--- FU.SPA/src/components/PostCard.jsx
+++ FU.SPA/src/components/PostCard.jsx
@@ -1,35 +1,35 @@
 import { Card, CardActions, Button, CardContent, Typography, Chip } from '@mui/material';
 
 const PostCard = ({ post }) => {
-    const dateTimeString = new Date(post.startTime)
-        .toLocaleString("en-US", { dateStyle: "medium", timeStyle: 'short' })
+  const dateTimeString = new Date(post.startTime)
+    .toLocaleString("en-US", { dateStyle: "medium", timeStyle: 'short' })
 
-    return (
-        <Card style={{ textAlign: "left" }} >
-            <CardContent style={{ width: "200pt", height: "250pt" }}>
-                <Typography variant="body2" color="text.secondary">
-                    by {post.creator}
-                </Typography>
-                <Typography variant="h5">
-                    {post.title}
-                </Typography>
-                <Typography variant="body1" >
-                    {dateTimeString}
-                </Typography>
-                <br />
-                <Typography variant="body2" color="text.primary">
-                    {post.description}
-                </Typography>
-                <br />
-                <div>
-                    {post.tags.map(t => <Chip key={t} label={t} variant="outlined" />)}
-                </div>
-            </CardContent>
-            <CardActions>
-                <Button size="large">View</Button>
-            </CardActions>
-        </Card>
-    )
+  return (
+    <Card style={{ textAlign: "left" }} >
+      <CardContent style={{ width: "200pt", height: "250pt" }}>
+        <Typography variant="body2" color="text.secondary">
+          by {post.creator}
+        </Typography>
+        <Typography variant="h5">
+          {post.title}
+        </Typography>
+        <Typography variant="body1" >
+          {dateTimeString}
+        </Typography>
+        <br />
+        <Typography variant="body2" color="text.primary">
+          {post.description}
+        </Typography>
+        <br />
+        <div>
+          {post.tags.map(t => <Chip key={t} label={t} variant="outlined" />)}
+        </div>
+      </CardContent>
+      <CardActions>
+        <Button size="large">View</Button>
+      </CardActions>
+    </Card>
+  )
 }
 
 export default PostCard
