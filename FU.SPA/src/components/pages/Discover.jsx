df43abb9b9cdc29cbe99cd681c333e076567da34
James Pretorius <jamesmpretorius@gmail.com>
2023-12-01 18:35:47 -0500

    Simplify discover

--- FU.SPA/src/components/pages/Discover.jsx
+++ FU.SPA/src/components/pages/Discover.jsx
@@ -13,16 +13,11 @@ const checkboxIconChecked = <CheckBoxIcon fontSize="small" />;
 export default function Discover() {
   const [posts, setPosts] = useState([]);
   const [searchText, setSearchText] = useState("");
-  const [gameOptions, setGameOptions] = useState([]);
   const [games, setGames] = useState([]);
 
   useEffect(() => {
-    GameService.searchGames("").then(games => setGameOptions(games));
-  }, []);
-
-  useEffect(() => {
     submitSearch();
-  }, [games]);
+  }, [games, searchText]);
 
   const submitSearch = async () => {
     const query = {
@@ -40,41 +35,54 @@ export default function Discover() {
         <Typography variant="h5">
           Filters
         </Typography>
-        <Autocomplete
-          multiple
-          value={games}
-          onChange={(e, v) => setGames(v)}
-          options={gameOptions}
-          disableCloseOnSelect
-          getOptionLabel={option => option.name}
-          renderOption={(props, option, { selected }) => (
-            <li {...props}>
-              <Checkbox
-                icon={checkboxIconBlank}
-                checkedIcon={checkboxIconChecked}
-                style={{ marginRight: 8 }}
-                checked={selected}
-              />
-              {option.name}
-            </li>
-          )}
-          renderInput={(params) => (
-            <TextField {...params} label="Games" placeholder="" />
-          )}
-        />
+        <GamesSelector onChange={(e, v) => setGames(v)} />
       </div>
       <div>
-        <SearchBar onSearchText={searchText} onSearchChange={t => setSearchText(t)} onSearchSubmit={submitSearch} />
+        <SearchBar onSearchSubmit={setSearchText} />
         <Posts posts={posts} />
       </div>
     </div >
   );
 }
 
-function SearchBar({ onSearchText, onSearchChange, onSearchSubmit }) {
+function GamesSelector({ onChange }) {
+  const [gameOptions, setGameOptions] = useState([]);
+
+  useEffect(() => {
+    GameService.searchGames("").then(games => setGameOptions(games));
+  }, []);
+
+  return (
+    <Autocomplete
+      multiple
+      onChange={onChange}
+      options={gameOptions}
+      disableCloseOnSelect
+      getOptionLabel={option => option.name}
+      renderOption={(props, option, { selected }) => (
+        <li {...props}>
+          <Checkbox
+            icon={checkboxIconBlank}
+            checkedIcon={checkboxIconChecked}
+            style={{ marginRight: 8 }}
+            checked={selected}
+          />
+          {option.name}
+        </li>
+      )}
+      renderInput={(params) => (
+        <TextField {...params} label="Games" placeholder="" />
+      )}
+    />
+  )
+}
+
+function SearchBar({ onSearchSubmit }) {
+  const [searchText, setSearchText] = useState("");
+
   function onKeyDown(event) {
     if (event.key === "Enter") {
-      onSearchSubmit()
+      onSearchSubmit(searchText)
     }
   }
 
@@ -84,8 +92,8 @@ function SearchBar({ onSearchText, onSearchChange, onSearchSubmit }) {
         id="outlined-basic"
         label="Search"
         variant="outlined"
-        value={onSearchText}
-        onChange={e => onSearchChange(e.target.value)}
+        value={searchText}
+        onChange={e => setSearchText(e.target.value)}
         onKeyDown={onKeyDown}
       />
     </div>
