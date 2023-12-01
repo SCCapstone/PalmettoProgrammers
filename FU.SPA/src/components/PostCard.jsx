f425dec96dcd32a3f11439086f2ff9ba11b24be0
James Pretorius <jamesmpretorius@gmail.com>
2023-12-01 13:19:57 -0500

    Update card design

---
+++ FU.SPA/src/components/PostCard.jsx
@@ -0,0 +1,35 @@
+import { Card, CardActions, Button, CardContent, Typography, Chip } from '@mui/material';
+
+const PostCard = ({ post }) => {
+    const dateTimeString = new Date(post.startTime)
+        .toLocaleString("en-US", { dateStyle: "medium", timeStyle: 'short' })
+
+    return (
+        <Card sx="text-align: left;" key={post.id}>
+            <CardContent sx="width: 200pt; height: 250pt">
+                <Typography variant="body2" color="text.secondary">
+                    by {post.creator}
+                </Typography>
+                <Typography variant="h5">
+                    {post.title}
+                </Typography>
+                <Typography variant="body1" >
+                    {dateTimeString}
+                </Typography>
+                <br />
+                <Typography variant="body2" color="text.primary">
+                    {post.description}
+                </Typography>
+                <br />
+                <div>
+                    {post.tags.map(t => <Chip label={t} variant="outlined" />)}
+                </div>
+            </CardContent>
+            <CardActions>
+                <Button size="large">View</Button>
+            </CardActions>
+        </Card>
+    )
+}
+
+export default PostCard
--- FU.SPA/src/components/Posts.jsx
+++ FU.SPA/src/components/Posts.jsx
@@ -1,34 +1,11 @@
 import React from 'react';
-import { Button, Card, CardActions, CardContent, Typography } from '@mui/material';
-
+import PostCard from './PostCard';
 
 const Posts = ({ posts }) => {
 
   return (
     <div style={{ display: 'flex', flexWrap: 'wrap', gap: '16px' }}>
-      {posts.map((post) => (
-        <Card key={post.id}>
-          {post.title}
-          <CardContent>
-            <Typography variant="body5" color="text.primary">
-              {post.creator}
-            </Typography>
-            <Typography gutterBottom variant="h3" component="div">
-              {post.title}
-            </Typography>
-            <Typography variant="body2" color="text.secondary">
-              {post.startTime}
-            </Typography>
-            <Typography variant="body1" color="black">
-              {post.description}
-            </Typography>
-          </CardContent>
-          <CardActions>
-            <Button size="small">Join</Button>
-            <Button size="small">View</Button>
-          </CardActions>
-        </Card>
-      ))}
+      {posts.map((post) => <PostCard key={post.id} post={post} />)}
     </div>
   );
 };
