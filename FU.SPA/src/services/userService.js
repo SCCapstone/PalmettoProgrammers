916db811b5eef86d8d4f948ceb0c3d7daa8cc0d9
James Pretorius <jamesmpretorius@gmail.com>
2023-12-02 10:54:58 -0500

    Add UserService

---
+++ FU.SPA/src/services/userService.js
@@ -0,0 +1,16 @@
+import AuthService from "./authService";
+
+const API_BASE_URL = import.meta.env.VITE_API_URL;
+
+const getConnectedPosts = async () => {
+  const response = await fetch(`${API_BASE_URL}/users/current/connected/posts`, { headers: {  ...AuthService.getAuthHeader() } });
+
+  if (!response.ok) {
+    throw new Error('Error getting posts');
+  }
+
+  return await response.json();
+};
+
+const UserService = { getConnectedPosts };
+export default UserService;
