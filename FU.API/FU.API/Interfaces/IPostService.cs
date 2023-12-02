7e54811a795295b293041b30b69508cca857f0ac
evan-scales <evan.m.scales@gmail.com>
2023-12-02 14:51:54 -0500

    145: Fix build error

--- FU.API/FU.API/Interfaces/IPostService.cs
+++ FU.API/FU.API/Interfaces/IPostService.cs
@@ -6,10 +6,9 @@ public interface IPostService : ICommonService
 {
     Task<Post> CreatePost(Post post);
 
-        Task<Post?> GetPost(int postId);
+    Task<Post?> GetPost(int postId);
 
-        Task JoinPost(int postId, ApplicationUser user);
+    Task JoinPost(int postId, ApplicationUser user);
 
-        Task LeavePost(int postId, ApplicationUser user);
-    }
+    Task LeavePost(int postId, ApplicationUser user);
 }
