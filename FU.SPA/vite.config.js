9ef093b914e439e1b7fda939c6c0e1d8f15befa7
evan-scales <evan.m.scales@gmail.com>
2023-12-02 16:32:23 -0500

    remove define

--- FU.SPA/vite.config.js
+++ FU.SPA/vite.config.js
@@ -4,7 +4,4 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  define: {
-    'import.meta.env.VITE_API_URL': JSON.stringify(process.env.VITE_API_URL),
-  },
 });
