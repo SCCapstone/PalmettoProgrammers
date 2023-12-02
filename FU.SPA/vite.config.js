dbda91b6eeeb8a73e44687ddc71e064c17f76e30
evan-scales <evan.m.scales@gmail.com>
2023-12-02 16:29:02 -0500

    add define

--- FU.SPA/vite.config.js
+++ FU.SPA/vite.config.js
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  define: {
+    'import.meta.env.VITE_API_URL': JSON.stringify(process.env.VITE_API_URL),
+  },
 });
