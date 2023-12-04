d4325671461e736460b3e5a7f43f681c0c5bb705
Jackson Williams <wjack1201@yahoo.com>
2023-12-03 21:30:47 -0500

    moved everything to the post webpage

---
+++ FU.SPA/src/config.js
@@ -0,0 +1,5 @@
+const config = {
+    API_URL: window.location.hostname === 'jolly-glacier-0ae92c40f.4.azurestaticapps.net' ? 'https://fuapi.azurewebsites.net/api' : import.meta.env.VITE_API_URL,
+};
+
+export default config;
---
+++ FU.SPA/src/pages/PostPage.jsx
@@ -0,0 +1,67 @@
+import React, { useState, useEffect } from 'react';
+import { Button, Card, CardHeader, CardContent, TextField, Link, Box, Container, Typography, 
+CssBaseline, Avatar, FormControlLabel, Icon, Grid, Checkbox } from '@mui/material';
+import { useParams } from 'react-router-dom';
+import { createTheme, ThemeProvider } from '@mui/material/styles'; 
+import PostService from '../services/PostService';
+const boxStyle = {
+    maxWidth: 600,
+    margin: 'auto',
+    marginTop: 16,
+    marginLeft: 0, 
+  };
+  
+const defaultTheme = createTheme();
+      const PostPage = () => {
+        const dateTimeString = new Date(post.startTime).toLocaleString('en-US', {
+            dateStyle: 'medium',
+            timeStyle: 'short',
+          });
+
+        const { postId } = useParams();
+        const [post, setPost] = useState(null);
+      
+        useEffect(() => {
+          const fetchPostDetails = async () => {
+            try {
+              const data = await PostService.getPostDetails(postId);
+              setPost(data);
+            } catch (error) {
+              console.error('Error fetching post details:', error);
+            }
+          };
+      
+          fetchPostDetails();
+        }, [postId]);
+  return (
+    <ThemeProvider theme={defaultTheme}>
+    <CssBaseline />
+    <Container maxWidth="lg">  
+    <Box style={boxStyle}>
+    <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'baseline' }}>
+        <Typography variant="h4" gutterBottom>
+          {post.title}
+        </Typography>
+            <Typography variant="h5" color="textSecondary">
+            {post.game}
+            </Typography>
+            <Typography variant="subtitle1" color="textSecondary">
+            by {post.creator}
+            </Typography>
+            <Typography variant="subtitle1" color="textSecondary">
+            {dateTimeString}
+            </Typography>
+        <Typography 
+        variant="body2" 
+        color="textSecondary" 
+        paragraph 
+        style={{ wordWrap:'break-word', textAlign:'left'}}>
+            {post.description}
+        </Typography>
+        </div>
+      </Box>
+    </Container>
+  </ThemeProvider>
+  ) 
+}
+export default PostPage;
