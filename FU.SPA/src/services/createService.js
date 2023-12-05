bc164a5634db79a6198a7901c5bbdf231820afda
epadams <epaadams2002@gmail.com>
2023-12-05 18:31:16 -0500

    Fix arguments issue

--- FU.SPA/src/services/createService.js
+++ FU.SPA/src/services/createService.js
@@ -3,14 +3,14 @@ const API_BASE_URL = config.API_URL;
 const LOCAL_STORAGE_TOKEN_KEY = 'token';
 
 // Create post request
-const createPost = async () => {
+const createPost = async (params) => {
   const response = await fetch(`${API_BASE_URL}/Posts`, {
     method: 'POST',
     headers: {
       'content-type': 'application/json',
       'Authorization': 'Bearer ' + localStorage.getItem(LOCAL_STORAGE_TOKEN_KEY)
     },
-    body: JSON.stringify(credentials)
+    body: JSON.stringify(params)
   });
 
   if (!response.ok) {
