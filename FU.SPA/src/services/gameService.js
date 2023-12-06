777f8ee4e30178b78985df4efe513de9bc113abd
epadams <epaadams2002@gmail.com>
2023-12-06 16:21:09 -0500

    Sorted create functions

--- FU.SPA/src/services/gameService.js
+++ FU.SPA/src/services/gameService.js
@@ -1,5 +1,6 @@
 import config from '../config';
 const API_BASE_URL = config.API_URL;
+const LOCAL_STORAGE_TOKEN_KEY = 'token';
 
 /*
   params = {
@@ -12,5 +13,24 @@ const searchGames = async (keyword) => {
   return await response.json();
 };
 
-const GameService = { searchGames };
+// Create game request
+const createGame = async (params) => {
+  const response = await fetch(`${API_BASE_URL}/Games`, {
+    method: 'POST',
+    headers: {
+      'content-type': 'application/json',
+      Authorization: 'Bearer ' + localStorage.getItem(LOCAL_STORAGE_TOKEN_KEY),
+    },
+    body: JSON.stringify(params),
+  });
+
+  if (!response.ok) {
+    throw new Error('Error in game creation');
+  }
+  const jsonResponse = await response.json();
+
+  console.log(jsonResponse);
+};
+
+const GameService = { searchGames, createGame };
 export default GameService;
--- FU.SPA/src/services/tagService.js
+++ FU.SPA/src/services/tagService.js
@@ -1,5 +1,6 @@
 import config from '../config';
 const API_BASE_URL = config.API_URL;
+const LOCAL_STORAGE_TOKEN_KEY = 'token';
 
 /*
   params = {
@@ -12,5 +13,24 @@ const searchTags = async (keyword) => {
   return await response.json();
 };
 
-const TagService = { searchTags };
+// Create tag request
+const createTag = async (params) => {
+  const response = await fetch(`${API_BASE_URL}/Tags`, {
+    method: 'POST',
+    headers: {
+      'content-type': 'application/json',
+      Authorization: 'Bearer ' + localStorage.getItem(LOCAL_STORAGE_TOKEN_KEY),
+    },
+    body: JSON.stringify(params),
+  });
+
+  if (!response.ok) {
+    throw new Error('Error in tag creation');
+  }
+  const jsonResponse = await response.json();
+
+  console.log(jsonResponse);
+};
+
+const TagService = { searchTags, createTag };
 export default TagService;
