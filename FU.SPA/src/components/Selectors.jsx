b9b4ecec3f0ed77b852a4a46972a08b493ed4754
James Pretorius <jamesmpretorius@gmail.com>
2023-12-06 22:10:13 -0500

    Extract selectors

---
+++ FU.SPA/src/components/Selectors.jsx
@@ -0,0 +1,73 @@
+import { useEffect, useState } from 'react';
+import GameService from '../services/gameService';
+import TagService from '../services/tagService';
+import { Autocomplete, Checkbox, TextField } from '@mui/material';
+import CheckBoxOutlineBlankIcon from '@mui/icons-material/CheckBoxOutlineBlank';
+import CheckBoxIcon from '@mui/icons-material/CheckBox';
+
+const checkboxIconBlank = <CheckBoxOutlineBlankIcon fontSize="small" />;
+const checkboxIconChecked = <CheckBoxIcon fontSize="small" />;
+
+export const GamesSelector = ({ onChange }) => {
+  const [gameOptions, setGameOptions] = useState([]);
+
+  useEffect(() => {
+    GameService.searchGames('').then((games) => setGameOptions(games));
+  }, []);
+
+  return (
+    <Autocomplete
+      multiple
+      onChange={onChange}
+      options={gameOptions}
+      disableCloseOnSelect
+      getOptionLabel={(option) => option.name}
+      renderOption={(props, option, { selected }) => (
+        <li {...props}>
+          <Checkbox
+            icon={checkboxIconBlank}
+            checkedIcon={checkboxIconChecked}
+            style={{ marginRight: 8 }}
+            checked={selected}
+          />
+          {option.name}
+        </li>
+      )}
+      renderInput={(params) => (
+        <TextField {...params} label="Games" placeholder="" />
+      )}
+    />
+  );
+};
+
+export const TagsSelector = ({ onChange }) => {
+  const [tagOptions, setTagOptions] = useState([]);
+
+  useEffect(() => {
+    TagService.searchTags('').then((tags) => setTagOptions(tags));
+  }, []);
+
+  return (
+    <Autocomplete
+      multiple
+      onChange={onChange}
+      options={tagOptions}
+      disableCloseOnSelect
+      getOptionLabel={(option) => option.name}
+      renderOption={(props, option, { selected }) => (
+        <li {...props}>
+          <Checkbox
+            icon={checkboxIconBlank}
+            checkedIcon={checkboxIconChecked}
+            style={{ marginRight: 8 }}
+            checked={selected}
+          />
+          {option.name}
+        </li>
+      )}
+      renderInput={(params) => (
+        <TextField {...params} label="Tags" placeholder="" />
+      )}
+    />
+  );
+}
