cb992e2e524615a35bbc6bf2ca043d88bb361358
Jackson Williams <wjack1201@yahoo.com>
2024-01-17 14:09:15 -0500

    avatar is generated based off of username

--- FU.SPA/src/components/Navbar.jsx
+++ FU.SPA/src/components/Navbar.jsx
@@ -2,6 +2,7 @@ import { Link, useMatch, useResolvedPath } from 'react-router-dom';
 import UserContext from '../context/userContext';
 import { useNavigate } from 'react-router-dom';
 import { useContext } from 'react';
+import Avatar from '@mui/material/Avatar';
 
 export default function Navbar() {
   const { user, logout } = useContext(UserContext);
@@ -11,10 +12,12 @@ export default function Navbar() {
     if (user) {
       return (
         <>
-          <p>{user.username}</p>
-          <li>
+        <li style={{ display: 'flex', alignItems: 'center' }}>
+            <Avatar {...stringAvatar(user.username)} sx={{ width: 33, height: 33, bgcolor: stringToColor(user.username) }}/>
+        </li>
+        <li style={{ display: 'flex', alignItems: 'center' }}>
           <button onClick={logout}>Logout</button>
-          </li>
+        </li>
         </>
       );
     } else {
@@ -60,4 +63,34 @@ function CustomLink({ to, children, ...props }) {
       </Link>
     </li>
   );
+}
+function stringToColor(string) {
+  let hash = 0;
+  let i;
+  for (i = 0; i < string.length; i += 1) {
+    hash = string.charCodeAt(i) + ((hash << 5) - hash);
+  }
+  let color = '#';
+  for (i = 0; i < 3; i += 1) {
+    const value = (hash >> (i * 8)) & 0xff;
+    color += `00${value.toString(16)}`.slice(-2);
+  }
+  return color;
+}
+
+function stringAvatar(name) {
+  // split  name by spaces and filter empty entries
+  let nameParts = name.split(' ').filter(Boolean);
+  // get the first letter of the first part
+  let initials = nameParts[0][0];
+  // if there is a second part to name
+  if (nameParts.length > 1) {
+    initials += nameParts[1][0];
+  }
+  return {
+    sx: {
+      bgcolor: stringToColor(name),
+    },
+    children: initials,
+  };
 }
