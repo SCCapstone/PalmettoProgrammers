e791e69b554c738590eb7959482938ccfe92a8ed
evan-scales <evan.m.scales@gmail.com>
2024-01-25 11:49:17 -0500

    update

--- FU.SPA/src/components/pages/Social.jsx
+++ FU.SPA/src/components/pages/Social.jsx
@@ -3,30 +3,13 @@ import UserService from '../../services/userService';
 import Posts from '../Posts';
 
 export default function Social() {
-  // const [currentTab, setCurrentTab] = useState('posts'); // ['posts', 'players', 'groups']
   const [posts, setPosts] = useState([]);
-  // const [groups, setGroups] = useState([]);
-  // const [players, setPlayers] = useState([]);
 
   useEffect(() => {
-    // if (currentTab === 'posts') {
-    //   UserService.getConnectedPosts().then(setPosts);
-    // } else if (currentTab === 'groups') {
-    //   UserService.getConnectedGroups().then(setGroups);
-    // } else if (currentTab === 'players') {
-    //   UserService.getConnectedPlayers().then(setPlayers);
-    // }
     UserService.getConnectedPosts().then(setPosts);
   }, []);
 
   const renderTabContent = () => {
-    // if (currentTab === 'posts') {
-    //   return <Posts posts={posts} />;
-    // } else if (currentTab === 'groups') {
-    //   return <p>Groups</p>;
-    // } else if (currentTab === 'players') {
-    //   return <p>Players</p>;
-    // }
     return <Posts posts={posts} />;
   };
 
