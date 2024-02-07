a091a08da019e6cd45c2957e63d34239869dcaf4
James Pretorius <jamesmpretorius@gmail.com>
2024-02-06 22:08:16 -0500

    Date range filter component setup

--- FU.SPA/src/components/pages/Discover.jsx
+++ FU.SPA/src/components/pages/Discover.jsx
@@ -1,3 +1,8 @@
+import { DatePicker } from '@mui/x-date-pickers/DatePicker';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+import dayjs from 'dayjs';
+import { useSearchParams } from 'react-router-dom';
 import { TextField, Typography } from '@mui/material';
 import { useEffect, useState } from 'react';
 import { TagsSelector, GamesSelector } from '../Selectors';
@@ -10,6 +15,17 @@ export default function Discover() {
   const [searchText, setSearchText] = useState('');
   const [games, setGames] = useState([]);
   const [tags, setTags] = useState([]);
+  const [searchParams, setSearchParams] = useSearchParams();
+  const [startDate, setStartDate] = useState(() => {
+    let startDateParam = searchParams.get('startDate');
+    if (startDateParam) return dayjs(startDateParam);
+    else return null;
+  });
+  const [endDate, setEndDate] = useState(() => {
+    let endDateParam = searchParams.get('endDate');
+    if (endDateParam) return dayjs(endDateParam);
+    else return null;
+  });
 
   useEffect(() => {
     const submitSearch = async () => {
@@ -25,18 +41,58 @@ export default function Discover() {
     submitSearch();
   }, [games, tags, searchText]);
 
+  // Update search params when startDate or endDate is updated
+  useEffect(() => {
+    setSearchParams(
+      (params) => {
+        if (startDate) params.set('startDate', startDate.toISOString());
+        if (endDate) params.set('endDate', endDate.toISOString());
+        return params;
+      },
+      { replace: true },
+    );
+  }, [startDate, endDate, setSearchParams]);
+
   return (
     <div className="page-content">
       <div className="sidebar" style={{ textAlign: 'left', minWidth: '200pt' }}>
         <Typography variant="h5">Filters</Typography>
         <GamesSelector onChange={(e, v) => setGames(v)} />
         <TagsSelector onChange={(e, v) => setTags(v)} />
+        <SelectDateRange
+          startDate={startDate}
+          endDate={endDate}
+          onStartDateChange={(newValue) => setStartDate(newValue)}
+          onEndDateChange={(newValue) => setEndDate(newValue)}
+        />
       </div>
       <div>
         <SearchBar onSearchSubmit={setSearchText} />
         <Posts posts={posts} />
       </div>
     </div>
+  );
+}
+
+function SelectDateRange({
+  onStartDateChange,
+  onEndDateChange,
+  startDate,
+  endDate,
+}) {
+  return (
+    <LocalizationProvider dateAdapter={AdapterDayjs}>
+      <DatePicker
+        label="From"
+        value={startDate}
+        onChange={(newValue) => onStartDateChange(newValue)}
+      />
+      <DatePicker
+        label="To"
+        value={endDate}
+        onChange={(newValue) => onEndDateChange(newValue)}
+      />
+    </LocalizationProvider>
   );
 }
 
