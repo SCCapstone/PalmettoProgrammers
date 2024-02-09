76fac2b21e106e4d67783acb39cec5c5e6d5e6c2
James Pretorius <jamesmpretorius@gmail.com>
2024-02-08 19:36:37 -0500

    Run formatter

--- FU.SPA/src/services/searchService.js
+++ FU.SPA/src/services/searchService.js
@@ -20,20 +20,24 @@ const searchPosts = async (query) => {
   if (query.startDate) {
     // get date from iso string
     // e.g. "2024-02-09T00:07:50.553Z" to "2024-02-09"
-    queryString += '&startOnOrAfterDate=' + query.startDate.toISOString().split('T')[0];
+    queryString +=
+      '&startOnOrAfterDate=' + query.startDate.toISOString().split('T')[0];
   }
   if (query.endDate) {
-    queryString += '&endOnOrBeforeDate=' + query.endDate.toISOString().split('T')[0];
+    queryString +=
+      '&endOnOrBeforeDate=' + query.endDate.toISOString().split('T')[0];
   }
   if (query.startTime) {
     // get time from iso string
     // e.g. "2024-02-09T00:07:50.553Z" to "00:07:50"
     queryString +=
-      '&startOnOrAfterTime=' + query.startTime.toISOString().split('T')[1].split('.')[0];
+      '&startOnOrAfterTime=' +
+      query.startTime.toISOString().split('T')[1].split('.')[0];
   }
   if (query.endTime) {
     queryString +=
-      '&endOnOrBeforeTime=' + query.endTime.toISOString().split('T')[1].split('.')[0];
+      '&endOnOrBeforeTime=' +
+      query.endTime.toISOString().split('T')[1].split('.')[0];
   }
 
   let authHeader = null;
