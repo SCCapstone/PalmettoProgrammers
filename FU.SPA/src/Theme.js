8210b8817b30babbf59ebbb1a5e53184feaa1e95
James Pretorius <jamesmpretorius@gmail.com>
2024-02-22 19:53:54 -0500

    Extract colors to const

--- FU.SPA/src/Theme.js
+++ FU.SPA/src/Theme.js
@@ -1,6 +1,9 @@
 import { createTheme } from '@mui/material/styles';
 
-const PRIMARY_MAIN = '#e354dc';
+const COLORS = {
+  PRIMARY_MAIN: '#e354dc',
+  SECONDARY_MAIN: '#4290f5',
+};
 
 const Theme = createTheme({
   palette: {
@@ -9,7 +12,7 @@ const Theme = createTheme({
       main: PRIMARY_MAIN,
     },
     secondary: {
-      main: '#42a5f5',
+      main: COLORS.SECONDARY_MAIN,
     },
   },
   components: {
@@ -21,7 +24,7 @@ const Theme = createTheme({
         root: {
           color: PRIMARY_MAIN,
           borderColor: PRIMARY_MAIN,
-          border: '2px solid ${PRIMARY_MAIN}',
+          border: '2px solid ${COLORS.PRIMARY_MAIN}',
         },
       },
     },
