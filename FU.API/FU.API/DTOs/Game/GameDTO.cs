82d5ce2cca94b0e2caca2662b933366b1517b67b
James Pretorius <jamesmpretorius@gmail.com>
2024-02-27 17:17:24 -0500

    Game titles can't be above 80 chars in API

--- FU.API/FU.API/DTOs/Game/GameDTO.cs
+++ FU.API/FU.API/DTOs/Game/GameDTO.cs
@@ -1,9 +1,12 @@
 ﻿namespace FU.API.DTOs.Game;
 
+using System.ComponentModel.DataAnnotations;
+
 public class GameDTO
 {
     public int Id { get; set; }
 
+    [StringLength(80, ErrorMessage = "Game name can't be longer than 80 chracters")]
     public string Name { get; set; } = string.Empty;
 
     public string? ImageUrl { get; set; }
--- FU.API/FU.API/DTOs/Game/UpdateGameDTO.cs
+++ FU.API/FU.API/DTOs/Game/UpdateGameDTO.cs
@@ -1,7 +1,10 @@
 ﻿namespace FU.API.DTOs.Game;
 
+using System.ComponentModel.DataAnnotations;
+
 public class UpdateGameDTO
 {
+    [StringLength(80, ErrorMessage = "Game name can't be longer than 80 chracters")]
     public string? Name { get; set; }
 
     public string? ImageUrl { get; set; }
