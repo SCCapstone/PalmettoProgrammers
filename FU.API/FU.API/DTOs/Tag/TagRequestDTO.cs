0f0aa949055167bba1f4cb09bd7fec8a6ad120f4
James Pretorius <jamesmpretorius@gmail.com>
2024-02-27 17:16:16 -0500

    Tags can't be above 20 chars in API

--- FU.API/FU.API/DTOs/Tag/TagRequestDTO.cs
+++ FU.API/FU.API/DTOs/Tag/TagRequestDTO.cs
@@ -1,9 +1,11 @@
 ﻿namespace FU.API.DTOs.Tag;
 
+using System.ComponentModel.DataAnnotations;
 using FU.API.Validation;
 
 public class TagRequestDTO
 {
     [NoSpaces]
+    [StringLength(20, ErrorMessage = "Tags can't be longer than 20 chracters")]
     public string Name { get; set; } = string.Empty;
 }
