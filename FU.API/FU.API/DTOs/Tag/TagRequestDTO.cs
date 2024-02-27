d688f5ff2ab2c23cbc04a45d9d91ffba463dcd35
James Pretorius <jamesmpretorius@gmail.com>
2024-02-27 18:25:52 -0500

    Tags must be lowercase

--- FU.API/FU.API/DTOs/Tag/TagRequestDTO.cs
+++ FU.API/FU.API/DTOs/Tag/TagRequestDTO.cs
@@ -2,10 +2,12 @@
 
 using System.ComponentModel.DataAnnotations;
 using FU.API.Validation;
+using FU.API.Helpers;
 
 public class TagRequestDTO
 {
     [NoSpaces]
+    [Lowercase]
     [StringLength(20, ErrorMessage = "Tags can't be longer than 20 chracters")]
     public string Name { get; set; } = string.Empty;
 }
---
+++ FU.API/FU.API/Helpers/Attributes.cs
@@ -0,0 +1,17 @@
+namespace FU.API.Helpers;
+
+using System.ComponentModel.DataAnnotations;
+
+public class LowercaseAttribute : ValidationAttribute
+{
+    protected override ValidationResult? IsValid(
+        object? value, ValidationContext validationContext)
+    {
+        if (value is string && ((string)value).ToLower() != (string)value)
+        {
+            return new ValidationResult("String must be lowercase");
+        }
+
+        return ValidationResult.Success;
+    }
+}
