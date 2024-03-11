9f173f28228ffdb8e586f1ad3733ed5d525869b7
evan-scales <evan.m.scales@gmail.com>
2024-03-11 16:36:45 -0400

    338: Removed some comments

--- FU.API/FU.API/Interfaces/ICommonService.cs
+++ FU.API/FU.API/Interfaces/ICommonService.cs
@@ -3,30 +3,11 @@
 using FU.API.Models;
 using System.Security.Claims;
 
-/// <summary>
-/// Interface for the common service.
-/// </summary>
 public interface ICommonService
 {
-    /// <summary>
-    /// Gets the current user from the claims.
-    /// </summary>
-    /// <param name="claims">The users claims.</param>
-    /// <returns>Potetinally current signed in user.</returns>
     Task<ApplicationUser?> GetCurrentUser(ClaimsPrincipal claims);
 
-    /// <summary>
-    /// Gets a user by their id.
-    /// </summary>
-    /// <param name="userId">The id of the user to get.</param>
-    /// <returns>Potetinally the user with the id.</returns>
     Task<ApplicationUser?> GetUser(int userId);
 
-    /// <summary>
-    /// Checks if a user has joined a post.
-    /// </summary>
-    /// <param name="userId">The id of the user.</param>
-    /// <param name="postId">The id of the post to check.</param>
-    /// <returns>If the user has joined the post.</returns>
     Task<bool> HasJoinedPost(int userId, int postId);
 }
--- FU.API/FU.API/Interfaces/ISearchService.cs
+++ FU.API/FU.API/Interfaces/ISearchService.cs
@@ -2,22 +2,9 @@
 
 using FU.API.Models;
 
-/// <summary>
-/// Interface for the search service.
-/// </summary>
 public interface ISearchService : ICommonService
 {
-    /// <summary>
-    /// Search for posts.
-    /// </summary>
-    /// <param name="query">The post query.</param>
-    /// <returns>The paginated posts, and total number of results.</returns>
     Task<(List<Post>, int TotalResults)> SearchPosts(PostQuery query);
 
-    /// <summary>
-    /// Search for users.
-    /// </summary>
-    /// <param name="query">The users query.</param>
-    /// <returns>The paginated users, and total number of results.</returns>
     Task<(List<UserProfile>, int TotalResults)> SearchUsers(UserQuery query);
 }
