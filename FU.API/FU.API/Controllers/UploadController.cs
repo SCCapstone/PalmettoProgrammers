910f391ca4f3139e2530e8b35d32386a690f6430
James Pretorius <jamesmpretorius@gmail.com>
2024-03-14 13:11:49 -0400

    Add upload avatar to server and process image

---
+++ FU.API/FU.API/Controllers/UploadController.cs
@@ -0,0 +1,96 @@
+namespace FU.API.Controllers;
+
+using Microsoft.AspNetCore.Mvc;
+using System.IO;
+using SkiaSharp;
+
+[ApiController]
+[Route("api/[controller]")]
+public class UploadController : ControllerBase
+{
+    [HttpPost]
+    [Route("avatar")]
+    public async Task<IActionResult> UploadProfileImage(IFormFile formFile)
+    {
+        SKBitmap bitmap;
+        using (var stream = formFile.OpenReadStream())
+        {
+            // TODO handle error
+            bitmap = SKBitmap.Decode(stream);
+        }
+
+        bitmap = CropToCenteredSquare(bitmap);
+        bitmap = ResizeToAvatarSize(bitmap);
+
+        using (MemoryStream memStream = new MemoryStream())
+        {
+            using SKManagedWStream wstream = new SKManagedWStream(memStream);
+
+            int quality = 80; // from 0-100
+            bool encodeSuccess = bitmap.Encode(wstream, SKEncodedImageFormat.Jpeg, quality);
+
+            memStream.Seek(0, SeekOrigin.Begin);
+
+            await SaveToTempFileAsync(memStream);
+        }
+
+        return Ok();
+    }
+
+    private static SKBitmap ResizeToAvatarSize(in SKBitmap bitmap)
+    {
+        SKImageInfo newResolution = new(200, 200);
+        return bitmap.Resize(newResolution, SKFilterQuality.High);
+    }
+
+    private static SKBitmap CropToCenteredSquare(in SKBitmap bitmap)
+    {
+        // Convert SKBitmap to SKPixmap
+        SKPixmap pixmap = new(bitmap.Info, bitmap.GetPixels());
+
+        int topOffset = 0;
+        int bottomOffest = 0;
+        int leftOffset = 0;
+        int rightOffset = 0;
+        if (pixmap.Height > pixmap.Width)
+        {
+            int excessHeight = pixmap.Height - pixmap.Width;
+            topOffset = excessHeight / 2;
+            bottomOffest = (excessHeight / 2) + (excessHeight % 2);
+        }
+        else if (pixmap.Height < pixmap.Width)
+        {
+            int excessWidth = pixmap.Width - pixmap.Height;
+            leftOffset = excessWidth / 2;
+            rightOffset = (excessWidth / 2) + (excessWidth % 2);
+        }
+
+        var croppedPixmap = pixmap.ExtractSubset(new SKRectI(leftOffset, topOffset, pixmap.Width - rightOffset, pixmap.Height - bottomOffest));
+
+        // Convert SKPixmap back to SKBitmap
+        SKBitmap newBitmap = new();
+        bool installSuccess = newBitmap.InstallPixels(croppedPixmap);
+
+        return newBitmap;
+    }
+
+    private static async Task<Guid> SaveToTempFileAsync(Stream stream)
+    {
+        Guid fileId = Guid.NewGuid();
+        string fileDirectory = $"{Path.GetTempPath()}/ForcesUnite/";
+
+        if (!Directory.Exists(fileDirectory))
+        {
+            Directory.CreateDirectory(fileDirectory);
+        }
+
+        string filePath = $"{fileDirectory}/{fileId.ToString()}";
+
+        using (var fileStream = System.IO.File.Create(filePath))
+        {
+            await stream.CopyToAsync(fileStream);
+        }
+
+        return fileId;
+    }
+}
