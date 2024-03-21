bb20d715ea732b80ee7242d84ea70803d1d79142
Jackson Williams <wjack1201@yahoo.com>
2024-03-21 18:55:44 -0400

    added count

--- FU.SPA/src/components/SearchResults.jsx
+++ FU.SPA/src/components/SearchResults.jsx
@@ -1,7 +1,7 @@
 import { Stack, Typography, Pagination } from '@mui/material';
 import SearchOffIcon from '@mui/icons-material/SearchOff';
 
-const searchResults = ({ page, totalResults, setPage }) => {
+const searchResults = ({ page, totalResults, queryLimit, setPage }) => {
   return totalResults === 0 ? (
     <div style={{ textAlign: 'center', color: 'violet', padding: '20px' }}>
       <SearchOffIcon style={{ fontSize: '64px' }} />
@@ -11,6 +11,7 @@ const searchResults = ({ page, totalResults, setPage }) => {
     <Stack spacing={2}>
       <Typography>Page: {page}</Typography>
       <Pagination
+        count={Math.ceil(totalResults / queryLimit)}
         page={page}
         onChange={(_, value) => setPage(value)}
         color="secondary"
