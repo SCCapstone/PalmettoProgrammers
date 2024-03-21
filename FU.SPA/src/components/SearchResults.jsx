c6966fc412435e781cf840ac6b8d5bff177a39c9
Jackson Williams <wjack1201@yahoo.com>
2024-03-21 11:56:36 -0400

    moved results and pagination to own component

---
+++ FU.SPA/src/components/SearchResults.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import { Stack, Typography, Pagination } from '@mui/material';
+import SearchOffIcon from '@mui/icons-material/SearchOff';
+
+const searchResults = ({
+  hasResults,
+  page,
+  totalResults,
+  queryLimit,
+  setPage,
+}) => {
+  return hasResults ? (
+    <Stack spacing={2}>
+      <Typography>Page: {page}</Typography>
+      <Pagination
+        count={Math.ceil(totalResults / queryLimit)}
+        page={page}
+        onChange={(_, value) => setPage(value)}
+        color="secondary"
+      />
+    </Stack>
+  ) : (
+    <div style={{ textAlign: 'center', color: 'violet', padding: '20px' }}>
+      <SearchOffIcon style={{ fontSize: '94px' }} />
+      <Typography variant="h4">No Results Found</Typography>
+    </div>
+  );
+};
+
+export default searchResults;
