ea56ed11251f0305b9ad15ba1e15e43f736ecc33
Jackson Williams <wjack1201@yahoo.com>
2024-03-21 16:44:09 -0400

    changed return stateement to if totalResults is equal to 0

--- FU.SPA/src/components/SearchResults.jsx
+++ FU.SPA/src/components/SearchResults.jsx
@@ -1,23 +1,23 @@
 import { Stack, Typography, Pagination } from '@mui/material';
 import SearchOffIcon from '@mui/icons-material/SearchOff';
 
-const searchResults = ({ page, totalResults, queryLimit, setPage }) => {
-  return totalResults ? (
+const searchResults = ({ page, totalResults, setPage }) => {
+  return totalResults === 0 ? (
+    <div style={{ textAlign: 'center', color: 'violet', padding: '20px' }}>
+      <SearchOffIcon style={{ fontSize: '64px' }} />
+      <Typography variant="h5">No Results Found</Typography>
+    </div>
+  ) : (
     <Stack spacing={2}>
       <Typography>Page: {page}</Typography>
       <Pagination
-        count={Math.ceil(totalResults / queryLimit)}
         page={page}
         onChange={(_, value) => setPage(value)}
         color="secondary"
       />
     </Stack>
-  ) : (
-    <div style={{ textAlign: 'center', color: 'violet', padding: '20px' }}>
-      <SearchOffIcon style={{ fontSize: '64px' }} />
-      <Typography variant="h5">No Results Found</Typography>
-    </div>
   );
+  
 };
 
 export default searchResults;
