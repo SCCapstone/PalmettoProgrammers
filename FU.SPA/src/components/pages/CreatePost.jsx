70966c652d6f74d6581a904655530f86098b18a2
Aaron Keys <106256122+AaronKeys@users.noreply.github.com>
2024-04-07 19:11:32 -0400

    Update CreatePost.jsx
    Combined an if and else if statement.

--- FU.SPA/src/components/pages/CreatePost.jsx
+++ FU.SPA/src/components/pages/CreatePost.jsx
@@ -74,11 +74,7 @@ export default function CreatePost() {
 
   // Handles game state error
   const handleGameChange = (e) => {
-    if (e === null || e.length === 0) {
-      setGameError('Game must be longer than 3 characters');
-      setGame(e);
-      setIsEnabled(false);
-    } else if (e.length < 3) {
+    if (e === null || e.length === 0 || e.length < 3) {
       setGameError('Game must be longer than 3 characters');
       setGame(e);
       setIsEnabled(false);
