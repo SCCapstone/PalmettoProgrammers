84e1aa42e150ce28a51846cf8c5de27cd1405ad3
James Pretorius <jamesmpretorius@gmail.com>
2024-04-10 19:20:31 -0400

    Chagne edit post to use PostForm

--- FU.SPA/src/components/pages/EditPost.jsx
+++ FU.SPA/src/components/pages/EditPost.jsx
@@ -1,7 +1,38 @@
-import Edit from '../Edit.jsx';
+import PostForm from '../PostForm';
+import PostService from '../../services/postService';
 import { useParams } from 'react-router';
+import { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 export default function EditPost() {
   const { postId } = useParams();
-  return <Edit postId={postId} />;
+  const [ogPost, setOgPost] = useState();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const getPost = async () => {
+      const ogPostDetails = await PostService.getPostDetails(postId);
+      setOgPost(ogPostDetails);
+    };
+    getPost();
+  }, [postId]);
+
+  const handleSubmit = async (newPost) => {
+    try {
+      await PostService.updatePost(newPost, postId);
+      alert('Post updated successfully!');
+      navigate(`/posts/${postId}`);
+    } catch (e) {
+      window.alert(e);
+      console.error(e);
+    }
+  };
+
+  return (
+    <PostForm
+      onSubmit={handleSubmit}
+      initialValue={ogPost}
+      submitButtonText="Update Post"
+    />
+  );
 }
