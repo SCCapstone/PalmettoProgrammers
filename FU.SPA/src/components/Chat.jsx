6c0f43edaa49d253823bcd875a2179baae45c3ea
AaronKeys <alkeys89@gmail.com>
2024-04-14 20:24:48 -0400

    small changes in the way the chat fills the screen for different sizes.

--- FU.SPA/src/components/Chat.jsx
+++ FU.SPA/src/components/Chat.jsx
@@ -125,7 +125,6 @@ export default function Chat({ chatId }) {
   }, [messages, prevScrollHeight]);
 
   // Use MUi card for chat
-  const chatHeight = window.innerHeight * 0.80;
 
   return (
     <Card
@@ -134,7 +133,7 @@ export default function Chat({ chatId }) {
         textAlign: 'left',
         minWidth: '300px',
         width: '100%',
-        height: chatHeight,
+        height: '65vh',
         display: 'flex',
         flexDirection: 'column',
         right: '5%',
